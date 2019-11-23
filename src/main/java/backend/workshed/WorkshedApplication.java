abfbc4c37a62e5d73d82c5a8b3cd26df996cc4b0
damian12332120 <damian.jedruchniewicz@gmail.com>
2019-11-23 09:48:03 +0100

    Initial commit

---
+++ src/main/java/backend/workshed/WorkshedApplication.java
@@ -0,0 +1,13 @@
+package backend.workshed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/workshed/workshed/WorkshedApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.workshed.workshed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
